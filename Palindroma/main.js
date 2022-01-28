9b895b2106c9429ad901d931b6f7f900324a6dc0
antoniogiachin <giachinantonio@outlook.com>
2022-01-28 17:42:15 +0100

    palindrome corretto e concluso

--- Palindroma/main.js
+++ Palindroma/main.js
@@ -11,10 +11,19 @@ function palindromCheck (word1, word2){
         //Ciclo i caratteri delle parole
         for(let i = 0; i < word1.length; i++){
 
-            /* i caratteri sono numerati come array da 0 in poi se nel ciclo corrispondono
-            sono palindrome altrimenti no */
+            /* rovescio sotto sopra la word2*/
 
-            if(word1[i] === word2[i]){
+            let split2 = word2.split('');
+            // Crea un Array con le lettere
+
+            // Rovesciamo Array
+            let reverse2 = split2.reverse();
+
+            // Converto di nuovo in stringa reverse2
+            let upsWord2 = reverse2.join('');
+
+
+            if(word1[i] === upsWord2[i]){
                 ritorno = 'sono palindrome!'
             } else {
                 ritorno = 'non sono palindrome!'
--- debug/debug.js
+++ debug/debug.js
@@ -1,20 +1,27 @@
-// Creo funzione per capire se parola inserita e' palindroma
-
 function palindromCheck (word1, word2){
 
     let ritorno;
     // le parole devono essere per forza lunghe uguali
     if (word1 < word2 || word1 > word2){
         ritorno= "non sono palindrome!"
-    } else{
+    } else {
 
         //Ciclo i caratteri delle parole
         for(let i = 0; i < word1.length; i++){
 
-            /* i caratteri sono numerati come array da 0 in poi se nel ciclo corrispondono
-            sono palindrome altrimenti no */
+            /* rovescio sotto sopra la word2*/
 
-            if(word1[i] === word2[i]){
+            let split2 = word2.split('');
+            // Crea un Array con le lettere
+
+            // Rovesciamo Array
+            let reverse2 = split2.reverse();
+
+            // Converto di nuovo in stringa reverse2
+            let upsWord2 = reverse2.join('');
+
+
+            if(word1[i] === upsWord2[i]){
                 ritorno = 'sono palindrome!'
             } else {
                 ritorno = 'non sono palindrome!'
@@ -30,42 +37,9 @@ function palindromCheck (word1, word2){
 
 }
 
-const button = document.getElementById('go');
 
-button.addEventListener('click',
+let firstWord = "cane"
+let secondWord = 'cane'
 
-    function(){
-        
-        // Raccolgo le parole inserite nel form
-        let firstWord = document.getElementById('word1').value;
-        let secondWord = document.getElementById('word2').value;
-        console.log(firstWord);
-        console.log(secondWord);
-
-        // Eseguo la verifica usando la funzione
-        let verification = palindromCheck(firstWord, secondWord);
-        console.log(verification);
-
-        // Scambio visibilita' dei due div row
-        const inputRow = document.getElementById('input-row');
-        inputRow.classList.add('d-none');
-
-        const resultRow = document.getElementById('result-row');
-        resultRow.classList.remove('d-none');
-
-        // Inietto contenuto in row2
-        const input = document.getElementById('input');
-
-        const wordList = document.getElementById('word-list');
-
-        const wordResult = document.getElementById('word-result');
-
-        input.innerHTML=`Hai inserito le seguenti parole: `;
-
-        wordList.innerHTML = `<li> ${firstWord}</li> <li> ${secondWord}</li>`;
-
-        wordResult.innerHTML = `Le due parole ${verification}`;
-    }
-
-
-);
+let verify = palindromCheck(firstWord, secondWord);
+console.log(verify);
